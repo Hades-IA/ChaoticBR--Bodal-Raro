ff4e4b5caab0808d3f5c5f97141a615e678cdaa7
Hades-IA <arilsonklaus@gmail.com>
2023-04-06 16:24:43 -0300

    cardtext body

--- public/js/index.js
+++ public/js/index.js
@@ -9,63 +9,69 @@ async function starter() {
       result = a.name >= b.name ? 1 : -1;
       return result;
     });
-    console.log(cards);
     var urlParams =
       location.search.length > 0 ? location.search.slice(1) : "p=1";
-
     var pageNum = urlParams.split("=")[1];
-    var pageLimit = Math.round(cards.length / 52);
-    if (pageNum > pageLimit || pageNum < 1) location.replace("?p=1");
-
-    let pagesList = [...document.getElementsByClassName("numerapage")];
-    let lastPages = [...document.getElementsByClassName("ultimapage")];
-    var pageListNum =
-      pageNum >= pageLimit || pageNum >= pageLimit - 2
-        ? pageNum - 4
-        : pageNum - 1;
-    console.log(pageListNum);
-    pagesList.forEach((link) => {
-      pageListNum++;
-      link.innerHTML = `
-     <a class=" ml-auto mr-2 numerapage" href="?p=${pageListNum}" class="mr-2">${pageListNum}</a>
-     `;
-    });
-
-    var lastPageNum = pageLimit - 3;
-    lastPages.forEach((link) => {
-      lastPageNum++;
-      link.innerHTML = ` <a class=" mr-2 ml-2 ultimapage" href="?p=${lastPageNum}">${lastPageNum}</a>`;
-    });
-
-    var loopCounter = 52 * (pageNum - 1);
-    var cardsRange = pageNum * 52;
-    var idElement = document.getElementById("scans");
-
-    for (let i = loopCounter; i < cardsRange; i++) {
-      try {
-        if (cards[i].name.length > 1) {
-          let card = cards[i];
-          idElement.innerHTML += ` <div id="${card.id}" onclick=pop_up(${card.id})>
-                <h6  >${card.name}</h6>
-                <img  src="${card.url}" SameSite=Lax crossorigin="anonymous"> <div class="popup">
-                      <span class="popuptext"">${card.bodybr}</span>
-                  </div> </img>
-
-            </div>`;
-        }
-      } catch (error) {
-        i = cardsRange;
-        console.log(error);
-      }
-    }
-
-    function pop_up(id) {
-      let cardpopup = document.getElementById(id);
-
-      cardpopup.getElementsByClassName("popuptext")[0].classList.toggle("show");
-    }
+    pageList(pageNum, cardsJson);
+    createCArdList(pageNum, cardsJson);
   } catch (error) {
     console.log(error);
+  }
+}
+
+function pageList(pageNum, cards) {
+  var pageLimit = Math.round(cards.length / 52);
+  if (pageNum > pageLimit || pageNum < 1) location.replace("?p=1");
+
+  let pagesList = [...document.getElementsByClassName("numerapage")];
+  let lastPages = [...document.getElementsByClassName("ultimapage")];
+  var pageListNum =
+    pageNum >= pageLimit || pageNum >= pageLimit - 2
+      ? pageNum - 4
+      : pageNum - 1;
+
+  pagesList.forEach((link) => {
+    pageListNum++;
+    link.innerHTML = `
+ <a class=" ml-auto mr-2 numerapage" href="?p=${pageListNum}" class="mr-2">${pageListNum}</a>
+ `;
+  });
+
+  var lastPageNum = pageLimit - 3;
+  lastPages.forEach((link) => {
+    lastPageNum++;
+    link.innerHTML = ` <a class=" mr-2 ml-2 ultimapage" href="?p=${lastPageNum}">${lastPageNum}</a>`;
+  });
+}
+function createCArdList(pageNum, cards) {
+  var loopCounter = 52 * (pageNum - 1);
+  var cardsRange = pageNum * 52;
+  var idElement = document.getElementById("scans");
+
+  for (let i = loopCounter; i < cardsRange; i++) {
+    try {
+      if (cards[i].name.length > 1) {
+        let card = cards[i];
+        idElement.innerHTML += ` <div id="${card.id}" onclick=pop_up(${card.id})>
+              <h6  >${card.name}</h6>
+              <img  src="${card.url}" SameSite=Lax crossorigin="anonymous"> <div class="popup">
+                    <span class="popuptext"">${card.bodybr}</span>
+                </div> </img>
+                <p>
+                ${card.bodybr}
+                </p>
+          </div>`;
+      }
+    } catch (error) {
+      i = cardsRange;
+      console.log(error);
+    }
+  }
+
+  function pop_up(id) {
+    let cardpopup = document.getElementById(id);
+
+    cardpopup.getElementsByClassName("popuptext")[0].classList.toggle("show");
   }
 }
 starter();
